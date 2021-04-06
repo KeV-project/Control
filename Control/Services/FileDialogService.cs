a9553f8307d7678b43098e7733a547db5a536a65
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-04-06 20:52:54 +0700

    feature(ControlView.Services): добавлен класс IFileDialogService

---
+++ Control/Services/FileDialogService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Windows;
+using ControlViewModel.Services;
+
+namespace ControlView.Services
+{
+	public class FileDialogService: IFileDialogService
+	{
+		public string FilePath { get; set; }
+
+        public bool AddFileDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FilePath = openFileDialog.FileName;
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- ControlViewModel/ControlViewModel.cs
+++ ControlViewModel/ControlViewModel.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ControlViewModel.Commands;
 
 namespace ControlViewModel
 {
-    public class ControlViewModel
+    public class ControlViewModel: ViewModelBase
     {
-
+        private RelayCommand _addFileCommand;
+        public RelayCommand AddFileCommand
+        {
+            get
+            {
+                return _addFileCommand ??
+                 (_addFileCommand = new RelayCommand(obj =>
+                 {
+                     
+                 }));
+            }
+        }
     }
 }
