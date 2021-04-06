178cc701eb7973e3aece61828a9b2b65aade2851
Kabanova Ekaterina <katovskaya009@gmail.com>
2021-04-06 20:09:24 +0700

    feature(ControlModel): добавлен класс Project

---
+++ ControlViewModel/Project.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlModel
+{
+    public class Project
+    {
+        private List<FileInfo> _files;
+
+        public FileInfo this[int index] => _files[index];
+
+        public Project()
+		{
+            _files = new List<FileInfo>();
+		}
+
+        public void AddFile(FileInfo file)
+		{
+            _files.Add(file);
+		}
+
+        public void RemoveFile(FileInfo file)
+		{
+            _files.Remove(file);
+		}
+    }
+}
